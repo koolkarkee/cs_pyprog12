d0128de3938ab62ea22188945e99532d899bc7c9
koolkarkee <koolkarkee@gmail.com>
2024-05-24 07:56:32 +0545

    single line & multi-line comments

---
+++ 01 hello.py
@@ -0,0 +1,20 @@
+print('hello world 1')
+
+print("hello world 2")
+
+print("""hello world 3""") 
+
+print('\nMy name is : \tMr. Bibek Karki')
+
+#print('this is a single line comment')
+
+'''
+print('hello world 1')
+print("hello world 2")
+print("""hello world 3""") 
+THis is multi line comment and the code block will not
+be interpreted
+'''
+
+print('code after multi line comment')
+
--- hello.py
+++
@@ -1,5 +0,0 @@
-print('hello world 1')
-
-print("hello world 1")
-
-print("""hello world 3""") 
