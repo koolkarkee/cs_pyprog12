0e7a7e83051bec25ed8c358981677fd0aa8e96e9
koolkarkee <koolkarkee@gmail.com>
2024-05-31 06:20:17 +0545

    del keyword in python

--- 02 variables.py
+++ 02 variables.py
@@ -15,9 +15,27 @@ def demo_variabe():
     #f-string 
     print(f"\ntype of pi = {type(pi)} and value of pi = {pi}")  
 
+def demo_del():
+    price : float = 20.4
+    print(price)
+    print(id(price))
+
+    #another variable with same value
+    price_2 = 20.4
+    print(id(price_2)) #output: references memory of price
+
+    price_3 = 20.5
+    print(id(price_3))
+
+    del(price)
+    #print(price)
+
+    print(price_2)
+
 #this is a main function 
 def main():
-    demo_variabe()
+    #demo_variabe()
+    demo_del()
 
 
 #call the main function 
